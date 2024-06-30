a3f98e602193625436769aafc886599b32c8cc22
hossein.naseri <hossein.naseri.info@gmail.com>
2024-06-30 12:23:55 +0330

    recursive binary search

--- 10-recursive-binary-search.js
+++ 10-recursive-binary-search.js
@@ -7,19 +7,16 @@
  */
 
 function recursiveBinarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-    while (left <= right) {
-        let middle = Math.floor((left + right) / 2);
-        if (target === arr[middle]) return middle;
-        if (target < arr[middle]) {
-            right = middle - 1
-        } else {
-            left = middle + 1
-        }
-    }
-    return -1
+    return search(arr, target, 0, arr.length - 1)
 };
 
-// Big-O = O(log n)      0                  middle                  11
-console.log(recursiveBinarySearch([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 100], 7));
+function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) return -1;
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) return middleIndex;
+    if (target > arr[middleIndex]) return search(arr, target, middleIndex + 1, rightIndex)
+    if (target < arr[middleIndex]) return search(arr, target, leftIndex, middleIndex - 1)
+}
+
+// Big-O = O(log n)               0                  middle                  11
+console.log(recursiveBinarySearch([10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 100], 18));
